9d9cb03f307515ce2f0c378afd9b1da524e23f66
Hazael Jiménez <viajero25057@innovaccion.mx>
2023-02-27 00:13:11 -0600

    Added Light and Dark Mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,22 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					200: '#ffdbc9',
+					500: '#974810',
+				},
+				secondary:{
+					200: '#ffdad9',
+					500: '#9c4146',
+				},	
+				tertiary:{
+					200: '#e7deff',
+					500: '#644bba',
+				},	
+			}
+		},
 	},
 	plugins: [],
 }
