ebc0dbf2d4924d140addb5af37ee0b871870b2cc
Hazael Jiménez <viajero25057@innovaccion.mx>
2023-02-27 18:00:13 -0600

    Added HeroHome component in Home UI

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -42,14 +42,14 @@ const Search: FC<Props> = ({
     <div className="w-4/5 flex justify-center">
       <div className="mb-3 xl:w-96">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-          <input
+          <input            
             type="search"
             value={searched}
             onChange={handledChange}
-            className="relative m-0 -mr-px block w-[1%] min-w-0 flex-auto rounded-l border border-solid border-tertiary-500 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-tertiary-200 dark:text-neutral-200 dark:placeholder:text-neutral-200"
-            placeholder="Search"
-            aria-label="Search"
-            aria-describedby="button-addon3"
+            className="relative m-0 -mr-px block w-[1%] min-w-0 flex-auto rounded border border-solid border-tertiary-500 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-tertiary-200 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+            placeholder="Find the perfect breed for you..."
+            aria-label="Search any breed or sub-breed"
+            aria-describedby="button-search"
           />
         </div>
       </div>
